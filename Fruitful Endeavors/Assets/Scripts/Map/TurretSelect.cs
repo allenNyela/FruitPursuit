4bd15e7b5130ea8ffff0edf6f8d8087a3470dfef
Nyela Allen <58515942+allenNyela@users.noreply.github.com>
2026-04-04 19:38:02 -0700

    added turret selector script and tooltip icons

---
+++ Fruitful Endeavors/Assets/Scripts/Map/TurretSelect.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TurretSelect : MonoBehaviour
+{
+    public GameObject Tooltip;
+    public bool tooltipPres = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tooltipPres)
+        {
+            Tooltip.transform.position = Input.mousePosition;
+        }
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        if (gameObject.layer == LayerMask.NameToLayer("Turret"))
+        {
+            Tooltip.SetActive(true);
+            tooltipPres = true;
+        }
+        
+    }
+
+    private void OnMouseExit()
+    {
+        if (gameObject.layer == LayerMask.NameToLayer("Turret"))
+        {
+            Tooltip.SetActive(false);
+            tooltipPres = false;
+        }
+    }
+}
