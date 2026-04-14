5053adb831a7086fe0c8390cd960be4df73f638c
Nyela Allen <58515942+allenNyela@users.noreply.github.com>
2026-04-14 14:45:16 -0700

    fixed chocolate bug

--- Fruitful Endeavors/Assets/Scripts/Map/Turret.cs
+++ Fruitful Endeavors/Assets/Scripts/Map/Turret.cs
@@ -88,7 +88,7 @@ public class Turret : MonoBehaviour
             {
                 return;
             }
-            if ((!(target.gameObject.GetComponent<EnemyFruitMesh>().chosenPrefab == 3)) && (bulletPrefab.GetComponent<Bullet>().type == Bullet.BulletType.Shield || bulletPrefab.GetComponent<Bullet>().type == Bullet.BulletType.Healing))
+            if ((!(target.gameObject.GetComponent<EnemyFruitMesh>().chosenPrefab == 2)) && (bulletPrefab.GetComponent<Bullet>().type == Bullet.BulletType.Shield || bulletPrefab.GetComponent<Bullet>().type == Bullet.BulletType.Healing))
             {
                 return;
             } else
